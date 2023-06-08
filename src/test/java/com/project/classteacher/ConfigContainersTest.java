682849529978b8bb8c7259c437b87d8945425e5c
nithack <andreynithack@gmail.com>
2023-06-08 14:30:04 -0300

    refactor(ConfigContainersTest.java): remove unused imports to improve code readability and maintainability

--- src/test/java/com/project/classteacher/ConfigContainersTest.java
+++ src/test/java/com/project/classteacher/ConfigContainersTest.java
@@ -1,8 +1,6 @@
 package com.project.classteacher;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.test.context.ActiveProfiles;
 
 @Import(MongoConfig.class)
