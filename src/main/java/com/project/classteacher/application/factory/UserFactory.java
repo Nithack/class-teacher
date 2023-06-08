54f5db1ebf1c0422ed7df0f38b3eade6bd00c670
nithack <andreynithack@gmail.com>
2023-06-08 20:32:59 -0300

    refactor(UserFactory.java): remove unnecessary default case in switch statement as it is unreachable and throws an exception

--- src/main/java/com/project/classteacher/application/factory/UserFactory.java
+++ src/main/java/com/project/classteacher/application/factory/UserFactory.java
@@ -5,16 +5,12 @@ import com.project.classteacher.domain.entity.Secretary;
 import com.project.classteacher.domain.entity.Teacher;
 import com.project.classteacher.domain.entity.User;
 
-import java.util.UUID;
-
-
 public class UserFactory {
 
     public static User createUser(String name, String email, String password, Roles role) {
         return switch (role) {
             case TEACHER -> createTeacher(name, email, password);
             case SECRETARY -> createSecretary(name, email, password);
-            default -> throw new IllegalArgumentException("Invalid role");
         };
     }
     private static Teacher createTeacher(String name, String email, String password){
