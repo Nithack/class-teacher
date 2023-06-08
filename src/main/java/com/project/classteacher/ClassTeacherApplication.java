89cfc347e6a7503d4708593dec11ecb04ed6732c
nithack <andreynithack@gmail.com>
2023-06-08 03:36:46 -0300

    feat: start project

---
+++ src/main/java/com/project/classteacher/ClassTeacherApplication.java
@@ -0,0 +1,13 @@
+package com.project.classteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassTeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassTeacherApplication.class, args);
+	}
+
+}
