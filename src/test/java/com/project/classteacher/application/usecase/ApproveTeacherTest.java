2b61dd4e3a0c4994987bbd5a0292f253342182f0
nithack <andreynithack@gmail.com>
2023-06-08 23:58:20 -0300

    feat(ApproveTeacher.java, ApproveTeacherTest.java): add new use case and test for approving a teacher in the system.

---
+++ src/test/java/com/project/classteacher/application/usecase/ApproveTeacherTest.java
@@ -0,0 +1,73 @@
+package com.project.classteacher.application.usecase;
+
+import com.project.classteacher.application.repository.ClassroomRepository;
+import com.project.classteacher.application.repository.UserRepository;
+import com.project.classteacher.config.decorators.ConfigContainersTest;
+import com.project.classteacher.domain.enums.Roles;
+import com.project.classteacher.util.builder.TestBuilderUtil;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DisplayName("Save Teacher Test")
+@ConfigContainersTest
+public class TeacherAprove {
+
+    @MockBean
+    private ClassroomRepository classroomRepository;
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @Autowired
+    private CreateTeacher createTeacher;
+
+    UUID DEFAULT_UUID;
+
+    @BeforeEach
+    public void setUp() {
+        this.DEFAULT_UUID = TestBuilderUtil.generateId();
+    }
+
+    @Test
+    @DisplayName("Should be created new teacher with information")
+    public void should_be_created_new_teacher_with_id() {
+
+        var newTeacher = TestBuilderUtil.createTeacher(
+                this.DEFAULT_UUID,
+                "Teacher 1",
+                "teacher1@gmail.com",
+                "123456"
+        );
+        Mockito.when(userRepository.saveTeacher(newTeacher)).thenReturn(newTeacher);
+        var teacherSaved = createTeacher.execute(newTeacher);
+        assertEquals(teacherSaved.getId(), this.DEFAULT_UUID);
+        assertEquals(teacherSaved.getName(), "Teacher 1");
+        assertEquals(teacherSaved.getEmail(), "teacher1@gmail.com");
+        assertEquals(teacherSaved.getPassword(), "123456");
+    }
+
+    @Test
+    @DisplayName("Should be created new teacher with teacher role")
+    public void should_be_created_new_teacher_with_teacher_role() {
+
+        var newTeacher = TestBuilderUtil.createTeacher(
+                this.DEFAULT_UUID,
+                "Teacher 1",
+                "teacher1@gmail.com",
+                "123456"
+        );
+        Mockito.when(userRepository.saveTeacher(newTeacher)).thenReturn(newTeacher);
+        var teacherSaved = createTeacher.execute(newTeacher);
+        assertEquals(teacherSaved.getRole(), Roles.valueOf("TEACHER"));
+    }
+
+}
